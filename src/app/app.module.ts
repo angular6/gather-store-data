4a5d9d29f9d3c684e15716e0899d935c8f17d476
Kevin Norton <rhchristian1@gmail.com>
2018-01-09 05:38:41 +0000

    pages: added "add-site"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AddDsuComponent } from './pages/add-dsu/add-dsu.component';
+import { AddSiteComponent } from './pages/add-site/add-site.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddDsuComponent
+    AddDsuComponent,
+    AddSiteComponent
   ],
   imports: [
     BrowserModule,
