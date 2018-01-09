59f6d795735b44fe44cf9a45f7325403aaf86b97
Kevin Norton <rhchristian1@gmail.com>
2018-01-09 05:51:03 +0000

    chore: added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AddDsuComponent } from './pages/add-dsu/add-dsu.component';
+import { AddSiteComponent } from './pages/add-site/add-site.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'add-dsu', component: AddDsuComponent },
+  { path: 'add-site', component: AddSiteComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
