361d56cace1155f320d931532f8c27183f66da5b
Kevin Norton <rhchristian1@gmail.com>
2018-01-09 05:39:21 +0000

    pages: added welcome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddDsuComponent } from './pages/add-dsu/add-dsu.component';
 import { AddSiteComponent } from './pages/add-site/add-site.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AddDsuComponent,
-    AddSiteComponent
+    AddSiteComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
