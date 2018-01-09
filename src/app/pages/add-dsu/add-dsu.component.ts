376542d89a7728d992695059de46d7668be96a6e
Kevin Norton <rhchristian1@gmail.com>
2018-01-09 16:24:30 +0000

    refactor (pages) : updated add dsu

--- src/app/pages/add-dsu/add-dsu.component.ts
+++ src/app/pages/add-dsu/add-dsu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -9,10 +9,6 @@ import { Title } from '@angular/platform-browser';
 export class AddDsuComponent implements OnInit {
 
   title: string = "Add DSU"
-  label1: string = "label1"
-  label2: string = "label2"
-  label3: string = "label3"
-
 
   constructor(private titleService: Title) {}
 
