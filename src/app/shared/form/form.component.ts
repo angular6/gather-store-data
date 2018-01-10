7232e6a65c747d5d3ff933a3dfdeb65d5b246067
Kevin Norton <rhchristian1@gmail.com>
2018-01-10 01:47:41 +0000

    refactor: Updated form

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -3,6 +3,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../../api.service';
 import * as _ from 'lodash'
 
+import { DsuMap } from '../models/dsu-map.model';
+import { Dsu } from '../models/dsu.model';
+import { Site } from '../models/site.model';
+
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -10,97 +15,112 @@ import * as _ from 'lodash'
 })
 export class FormComponent implements OnInit {
 
-  @Input() title: string
   formsObject: any = {}
-  formsArray: any = []
+  formsArray: FormGroup[] = []
   lastid: number = 0
+  switchInputs: boolean
+  dsus: any[]
 
+  @Input() title: string
   @Input() label1: string
   @Input() label2: string
   @Input() label3: string
   @Input() formType: string
-  switchInputs: boolean
-  dsus: any[]
 
   constructor(
     private apiService: ApiService
   ) {}
 
-  ngOnInit() {
-    this.addForm()
-    this.formsObjectToArray()
-    if (this.formType == "site") this.switchInputs = true
-    this.getDsus()
+
+  private formsObjectToArray(): void {
+    this.formsArray = _.values(this.formsObject)
   }
 
 
+  public createNewFormGroup(): FormGroup {
 
 
-  submit(x) {
+    return new FormGroup({
+      control1: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(10),
+      ]),
+      control2: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(20),
+      ]),
+      control3: new FormControl(null,
+        (this.formType == "site") ? [Validators.required] : null
+      ),
+    });
+
+  }
+
+
+  public submitForm(formGroup: FormGroup): void {
 
     if (this.formType == "dsu") {
-      var sendObject = { name: x.value.input1, description: x.value.input2, cert: x.value.input3 }
-      this.apiService.post(sendObject).subscribe()
+      var dsu: Dsu = { name: formGroup.value.control1, description: formGroup.value.control2, cert: formGroup.value.control3 }
+      this.apiService.putDsu(dsu).subscribe()
     }
     if (this.formType == "site") {
-      var sendObject2 = { name: x.value.input1, description: x.value.input2, dsuId: x.value.input3 }
-      this.apiService.post2(sendObject2).subscribe()
+      var site: Site = { name: formGroup.value.control1, description: formGroup.value.control2, dsuId: formGroup.value.control3 }
+      this.apiService.putSite(site).subscribe()
     }
-
 
   }
 
 
 
-  getDsus() {
-    this.apiService.get().subscribe(x => {
+
+  public getDsus(): void {
+    this.apiService.getDsus().subscribe((res: DsuMap) => {
       let dsus = []
-      Object.keys(x).map(function(objectKey, index) { dsus.push(x[objectKey]) });
+      Object.keys(res).map(function(objectKey, index) { dsus.push(res[objectKey]) });
       this.dsus = dsus
     })
   }
 
 
 
-  addForm() {
+  public addForm(): void {
+
     this.lastid++;
     let newForm = this.createNewFormGroup()
     newForm["_id"] = this.lastid
     this.formsObject[this.lastid] = newForm
     this.formsObjectToArray()
+
   }
 
-  removeThisForm(x) {
-    delete this.formsObject[x]
+
+  public removeThisForm(_id: number): void {
+
+    delete this.formsObject[_id]
     this.formsObjectToArray()
 
   }
 
-  resetForms() {
-    Object.keys(this.formsObject).forEach((x: any) => { if (x != 1) { delete this.formsObject[x] } })
+
+  public resetForms(): void {
+
+    Object.keys(this.formsObject).forEach((key: string) => { if (key != "1") { delete this.formsObject[key] } })
     this.formsObjectToArray()
-  }
-
-  formsObjectToArray() { this.formsArray = _.values(this.formsObject) }
-
-
-  createNewFormGroup() {
-
-    return new FormGroup({
-      input1: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(10),
-      ]),
-      input2: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(20),
-      ]),
-      input3: new FormControl(null, [
-        //Validators.required,
-      ]),
-    });
 
   }
+
+
+  ngOnInit() {
+
+    this.addForm();
+    this.formsObjectToArray();
+    if (this.formType == "site") {
+      this.switchInputs = true;
+      this.getDsus();
+    }
+
+  }
+
 
 
 }
