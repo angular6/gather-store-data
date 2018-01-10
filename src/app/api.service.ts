63aa316b8b73e31921d67d03c9ad1c5901e625e7
Kevin Norton <rhchristian1@gmail.com>
2018-01-10 21:32:48 +0000

    chore: updated api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+//import { map } from 'rxjs/operators';
 
 import { Dsu } from './shared/models/dsu.model';
 import { Site } from './shared/models/site.model';
