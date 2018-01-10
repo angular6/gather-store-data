ea45b511ef2afe729e64d967a7eff06e0ce1d9fb
Kevin Norton <rhchristian1@gmail.com>
2018-01-10 01:49:37 +0000

    refactor: updated add site

--- src/app/pages/add-site/add-site.component.ts
+++ src/app/pages/add-site/add-site.component.ts
@@ -4,16 +4,11 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-add-site',
   templateUrl: './add-site.component.html',
-  styleUrls: ['./add-site.component.scss']
-})
+  styleUrls: ['./add-site.component.scss']})
+
 export class AddSiteComponent implements OnInit {
 
   title: string = "Add Site"
-  formType: string = "site"
-  label1: string = "label1"
-  label2: string = "label2"
-  label3: string = "label3"
-
 
   constructor(
     private titleService: Title
