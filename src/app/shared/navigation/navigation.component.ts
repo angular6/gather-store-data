5d525868ee5d79ef0be89e6e115cadbb407131b4
Kevin Norton <rhchristian1@gmail.com>
2018-01-10 21:39:48 +0000

    shared (navigation): highlight buttons when on corresponding page

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationStart} from '@angular/router'
 import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-navigation',
@@ -9,9 +11,38 @@ import { Title } from '@angular/platform-browser';
 export class NavigationComponent implements OnInit {
 
   headerTitle: string = "Sample"
+  routerLink: string
 
-  constructor(private titleService: Title) {}
+  buttons: any = [
+    { id: 0, title: "Welcome", routerLink: "" },
+    { id: 1, title: "Add DSU", routerLink: "add-dsu" },
+    { id: 2, title: "Add Site", routerLink: "add-site" }
+  ]
 
-  ngOnInit() {  }
+  highlightThisButton() {
+    for (let i = 0; i < this.buttons.length; i++) {
+      if (this.buttons[i].routerLink == this.routerLink) {
+        return this.buttons[i].id
+      }
+    }
+  }
+
+  constructor(
+    private router: Router,
+    private title: Title
+  ) {
+
+    this.router.events.subscribe((x) => {
+      if (x instanceof ActivationStart) {
+        window.scrollTo(0, 0)
+        this.routerLink = x.snapshot.routeConfig.path
+        this.title.setTitle(x.snapshot.data.title)
+      }
+    })
+  }
+
+  ngOnInit() {
+
+  }
 
 }
